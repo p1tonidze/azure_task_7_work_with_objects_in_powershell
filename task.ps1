3f83eb236f79cf40e08cb4b0b1b1df11cfe3debc
p1tonidze <p1t0nidze422@gmail.com>
2024-12-04 12:16:40 +0200

    Solution

--- task.ps1
+++ task.ps1
@@ -1,1 +1,20 @@
-# Write your code here
+$PathData = Get-ChildItem ./data
+$ResultList = @()
+
+foreach ($file in $PathData)
+{
+    $content = Get-Content $file.FullName | ConvertFrom-Json
+    foreach ($data in $content)
+    {
+        if ($data.Name -eq "Standard_B2pts_v2")
+        {
+            $ResultList += $file.BaseName
+        }
+    }
+}
+
+# Запись результата в файл один раз после обработки всех файлов
+$ResultList | ConvertTo-Json | Out-File -FilePath ./result.json -Encoding utf8
+
+# Выводим список регионов в консоль
+$ResultList | ForEach-Object { Write-Host $_}
